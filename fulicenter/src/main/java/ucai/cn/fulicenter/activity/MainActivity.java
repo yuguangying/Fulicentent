1109d3271c41183f03ec717dd5ea5a6c545c88c7
yuguangying <1847266416@qq.com>
2016-10-14 20:49:19 +0800

    一些小修改

--- fulicenter/src/main/java/ucai/cn/fulicenter/activity/MainActivity.java
+++ fulicenter/src/main/java/ucai/cn/fulicenter/activity/MainActivity.java
@@ -2,11 +2,14 @@ package ucai.cn.fulicenter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import ucai.cn.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    RadioButton mnew_goods,mboutique,mcategory,mpersonal_center,mcart;
+    TextView mcount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
