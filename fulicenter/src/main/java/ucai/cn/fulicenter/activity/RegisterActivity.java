8301cf725de4237a242653ddea3f973e857a26f1
yuguangying <1847266416@qq.com>
2016-10-21 13:19:06 +0800

    用户与密码的非空判断并符合规定的格式

--- fulicenter/src/main/java/ucai/cn/fulicenter/activity/RegisterActivity.java
+++ fulicenter/src/main/java/ucai/cn/fulicenter/activity/RegisterActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -47,7 +50,45 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
                 break;
             case R.id.register_button:
-               
+                if ("".equals(registerName.getText().toString().trim())) {
+                    registerName.setError("用户名不能为空");
+                    registerName.requestFocus();
+                    return;
+                }
+//                第一种
+//                //1.将给定的正则表达式编译到模式中。
+//                Pattern p = Pattern.compile("^[a-zA-Z][a-zA-Z0-9]{1,9}$");
+//                //2.创建匹配给定输入与此模式的匹配器。
+//                Matcher m = p.matcher(registerName.getText().toString());
+//                //3.返回是否匹配
+//                boolean b = m.matches();
+//                第二种(与第一种差不多)
+                boolean b = registerName.getText().toString().matches("^[a-zA-Z][a-zA-Z0-9]{1,9}$");
+                if (b){
+                    registerName.setError("用户名必须首位为字母，4-11位");
+                    CommonUtils.showShortToast("用户名必须前三位为字母，4-10位");
+                    registerName.requestFocus();
+                    return;
+                }
+                if ("".equals(regiserNick.getText().toString().trim())) {
+                    regiserNick.setError("昵称不能为空");
+                    regiserNick.requestFocus();
+                    return;
+                }
+                if ("".equals(regiterPassword.getText().toString().trim())) {
+                    regiserNick.setError("密码不能为空");
+                    regiterPassword.requestFocus();
+                    return;
+                }
+                if ("".equals(registerPasswordTwo.getText().toString().trim())) {
+                    regiserNick.setError("密码确认不能为空");
+                    return;
+                }
+                if (!("".equals(registerName.getText().toString().trim()))&&regiterPassword.getText().toString().equals(registerPasswordTwo.getText().toString())) {
+                    CommonUtils.showShortToast("确认密码不一致");
+                    registerPasswordTwo.requestFocus();
+                    return;
+                }
                 break;
         }
     }
