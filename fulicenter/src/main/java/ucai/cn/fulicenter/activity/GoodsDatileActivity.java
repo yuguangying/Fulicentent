68fdd6689c5329ec8f42c154c6e4092864447d6a
yuguangying <1847266416@qq.com>
2016-10-26 18:27:57 +0800

    一键分享功能

--- fulicenter/src/main/java/ucai/cn/fulicenter/activity/GoodsDatileActivity.java
+++ fulicenter/src/main/java/ucai/cn/fulicenter/activity/GoodsDatileActivity.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import ucai.cn.fulicenter.FuLiCenterApplication;
 import ucai.cn.fulicenter.I;
 import ucai.cn.fulicenter.R;
@@ -69,7 +71,7 @@ public class GoodsDatileActivity extends BaseActivity {
             finish();
         }
         context = this;
-            updateLike();
+        updateLike();
         super.onCreate(savedInstanceState);
     }
 
@@ -134,7 +136,7 @@ public class GoodsDatileActivity extends BaseActivity {
                 finish();
                 break;
             case R.id.share:
-
+                showShare();
                 break;
             case R.id.like:
                 isCollect();
@@ -192,16 +194,16 @@ public class GoodsDatileActivity extends BaseActivity {
         GoodsDao.deleteCollect(context, goodsid, FuLiCenterApplication.getUser().getMuserName(), new OkHttpUtils.OnCompleteListener<MessageBean>() {
             @Override
             public void onSuccess(MessageBean result) {
-                if (result.isSuccess()){
+                if (result.isSuccess()) {
                     like.setChecked(false);
-                }else {
+                } else {
                     CommonUtils.showLongToast("删除失败");
                 }
             }
 
             @Override
             public void onError(String error) {
-                Log.i("main", "onError: "+error);
+                Log.i("main", "onError: " + error);
                 CommonUtils.showLongToast(error);
             }
         });
@@ -256,5 +258,36 @@ public class GoodsDatileActivity extends BaseActivity {
                 return true;
         }
         return false;
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
     }
 }
